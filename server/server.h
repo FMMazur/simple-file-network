ccbedfda30b8acfbbc6a2db80459f411f38fb499
Felipe Muñoz Mazur <felipe.mazur@yandeh.com.br>
2022-10-25 15:42:49 -0400

    fix(server): path argument.
    When `path` is `NULL`, use default path.

--- server/server.h
+++ server/server.h
@@ -44,59 +44,64 @@
 typedef struct dirent dirent;
 
 typedef struct {
-    u16 port;
-    char* root_path;
+  u16 port;
+  char *root_path;
 } arguments_t;
 
 typedef struct {
-    int total;
-    char* root_path;
+  int total;
+  char *root_path;
 
-    dirent **directories;
+  dirent **directories;
 } server_info_t;
 
-static error_t parse_opt(int key, char *arg, struct argp_state *state) {
-    arguments_t *arguments = (arguments_t*) state->input;
-    switch (key) {
-        case 'p': {
-            if (arg == NULL)
-                return 0;
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+  arguments_t *arguments = (arguments_t *) state->input;
+  switch (key) {
+    case 'p': {
+      if (arg == NULL)
+        return 0;
 
-            uintmax_t port = strtoumax(arg, NULL, 10);
+      uintmax_t port = strtoumax(arg, NULL, 10);
 
-            if (port == UINTMAX_MAX && errno == ERANGE) {
-                return 0;
-            }
+      if (port == UINTMAX_MAX && errno == ERANGE) {
+        return 0;
+      }
 
-            arguments->port = (u16) port;
-        }
-        break;
-        case ARGP_KEY_ARG: {
-            free(arguments->root_path);
-            arguments->root_path = realpath(arg, NULL);
-        }
-        break;
-        default: return ARGP_ERR_UNKNOWN;
+      arguments->port = (u16) port;
     }
+      break;
+    case ARGP_KEY_ARG: {
+      free(arguments->root_path);
 
-    return 0;
+      char *path = realpath(arg, NULL);
+      if (path)
+        arguments->root_path = path;
+    }
+      break;
+    default: return ARGP_ERR_UNKNOWN;
+  }
+
+  return 0;
 }
 
 static const char doc[] = DOCUMENTATION("this is a simple ftp server.");
 static const char args_doc[] = DOCUMENTATION("PATH");
 static const struct argp_option argument_options[] = {
-        { .name = "port", .key = 'p', .arg = "PORT", .flags = OPTION_ARG_OPTIONAL, .doc = "Change the ftp port. Default: " STR(DEFAULT_PORT) },
-        { 0 },
+    {.name = "port", .key = 'p', .arg = "PORT", .flags = OPTION_ARG_OPTIONAL, .doc = "Change the ftp port. Default: " STR(
+        DEFAULT_PORT)},
+    {0},
 };
 
-static struct argp argp = { argument_options, parse_opt, args_doc, doc, 0, 0, 0 };
+static struct argp argp = {argument_options, parse_opt, args_doc, doc, 0, 0, 0};
 
-int server_handle_connection(server_info_t* info, int socket);
-static int server_send_check(int socket, char* writeBuffer, int code, const char *message);
+int server_handle_connection(server_info_t *info, int socket);
+static int server_send_check(int socket, char *writeBuffer, int code, const char *message);
 void server_info_clear(server_info_t *info);
 void server_path_scan(server_info_t *serverInfo);
 
-int server_dir_remove_depth_cb(const char *fpath, const struct stat *sb, int typeFlag, struct FTW* ftw);
+int server_dir_remove_depth_cb(const char *fpath, const struct stat *sb, int typeFlag, struct FTW *ftw);
 
 int server_rmrf(char *path);
 
